a54f74decab96d5eaa3a3a147723354bee911dc5
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2024-01-02 22:33:49 +0300

    bitti

--- final.c
+++ final.c
@@ -1,11 +1,16 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int bakiye = 0;
+char ad[50];
+char soyad[50];
+int ekleme = 0;
+int durumNo, ogrenciDurumu = 0;
 
 void kayit() {
-    char ad[50];
-    char soyad[50];
+    
 
     printf("Lutfen adinizi giriniz: ");
     scanf("%s", ad);
@@ -15,8 +20,6 @@ void kayit() {
 }
 
 void bakiyeYukle() {
-    int ekleme = 0;
-    
     printf("Mevcut bakiyeniz: %d TL\n", bakiye);
     printf("Lutfen yuklemek istediginiz degeri giriniz: ");
     scanf("%d", &ekleme);
@@ -27,31 +30,33 @@ void bakiyeYukle() {
 }
 
 void durumGuncelleme() {
-    int durumNo, ogrenciDurumu;
-
     printf("Ogrenci iseniz 1, sivil iseniz 2, emekli iseniz 3 tusuna basiniz: ");
     scanf("%d", &durumNo);
 
     switch (durumNo) {
-        case 1:
-            printf("Universitede ogrenci iseniz 1, degil iseniz 0 tusuna basiniz: ");
-            scanf("%d", &ogrenciDurumu);
+    case 1:
+        printf("Universitede ogrenci iseniz 1, degil iseniz 0 tusuna basiniz: ");
+        scanf("%d", &ogrenciDurumu);
 
-            if (ogrenciDurumu == 1)
-                printf("Universitede ogrencisiniz ve odemeniz gereken ucret 6 TL'dir\n");
-            else
-                printf("Universitede ogrenci degilsiniz ve odemeniz gereken ucret 6 TL'dir\n");
-            break;
-        case 2:
-            printf("Sivilsiniz ve odemeniz gereken ucret 10 TL'dir\n");
-            break;
-        case 3:
-            printf("Emeklisiniz ve ucret odemenize gerek yok\n");
-            break;
-        default:
-            printf("Yanlis bir deger girdiniz\n");
-            break;
+        if (ogrenciDurumu == 1)
+            printf("Universitede ogrencisiniz ve odemeniz gereken ucret 6 TL'dir\n");
+        else
+            printf("Universitede ogrenci degilsiniz ve odemeniz gereken ucret 6 TL'dir\n");
+        break;
+    case 2:
+        printf("Sivilsiniz ve odemeniz gereken ucret 10 TL'dir\n");
+        break;
+    case 3:
+        printf("Emeklisiniz ve ucret odemenize gerek yok\n");
+        break;
+    default:
+        printf("Yanlis bir deger girdiniz\n");
+        break;
     }
+}
+
+void bilgileriGoster() {
+    printf("Adiniz: %s\nSoyadiniz: %s\nDurumunuz:\n(1=OGRENCI\n2=SIVIL\n3=EMEKLI) %d\nMevcut Bakiyeniz: %d TL\n", ad, soyad, ogrenciDurumu, bakiye);
 }
 
 int main() {
@@ -62,27 +67,33 @@ int main() {
         printf("1. Kayit yapmak icin\n");
         printf("2. Bakiye yuklemek icin\n");
         printf("3. Durum guncellemek icin\n");
+        printf("4. Mevcut bilgileri gormek icin\n");
         printf("Seciminizi giriniz: ");
         scanf("%d", &secim);
 
         switch (secim) {
-            case 1:
-                printf("Kayit olma islemini sectiniz\n");
-                kayit();
-                break;
-            case 2:
-                printf("Bakiye yukleme islemini sectiniz\n");
-                bakiyeYukle();
-                break;
-            case 3:
-                printf("Durum guncelleme islemini sectiniz\n");
-                durumGuncelleme();
-                break;
-            default:
-                printf("Gecersiz bir sayi sectiniz\n");
-                break;
+        case 1:
+            printf("Kayit olma islemini sectiniz\n");
+            kayit();
+            break;
+        case 2:
+            printf("Bakiye yukleme islemini sectiniz\n");
+            bakiyeYukle();
+            break;
+        case 3:
+            printf("Durum guncelleme islemini sectiniz\n");
+            durumGuncelleme();
+            break;
+        case 4:
+            printf("Mevcut bilgileri gorme islemini sectiniz\n");
+            bilgileriGoster();
+            break;
+        default:
+            printf("Gecersiz bir sayi sectiniz\n");
+            break;
         }
     }
 
     return 0;
 }
+
